ae26b200042fda8dd48bbb314eb55f52a1bd63ca
Advaith Venkatakrishnan <advaith96@gmail.com>
2022-01-25 18:42:25 -0500

    added tailwind

---
+++ src/components/TaxModel.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>Tax Model</h1>
+
+    <h2>Earnings</h2>
+    <div>
+      <h4>Group Term Life</h4>
+      <input type="text">
+    </div>
+    <h4>Salary</h4>
+  
+    <h2>Deductions</h2>
+    <h4>401k</h4>
+
+    <h2>Taxes</h2>
+    <h4>Federal Taxes</h4>
+    <h4>State Taxes</h4>
+    <h4>Local Taxes</h4>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'TaxModel'
+})
+</script>
